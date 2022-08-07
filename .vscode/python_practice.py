d17a98899b9c172d2980e2c6d4168c3928da4913
intmainvoid99 <dreami690515@gmail.com>
2022-08-07 23:49:33 +0900

    first commit

--- .vscode/python_practice.py
+++ .vscode/python_practice.py
@@ -1,6 +1,6 @@
 # 사이트별 비밀번호 만들기
 pagename = input("사이트의 주소를 입력하세요 : ")
-cuthttps = pagename[7:]
+cuthttps = pagename.replace("http://","")
 findot = cuthttps.find('.')
 sitename = cuthttps[:findot]
 password = sitename[:3] + str(len(sitename)) + str(sitename.count('e')) + '!'
