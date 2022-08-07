cf4086a1894a6cbfc3d25d8f869d56e95aa4718d
intmainvoid99 <dreami690515@gmail.com>
2022-08-07 23:42:28 +0900

    first commit

--- .vscode/first_step.py
+++ .vscode/first_step.py
@@ -1,1 +1,8 @@
-print("first step to git")
+# 사이트별 비밀번호 만들기
+pagename = input("사이트의 주소를 입력하세요 : ")
+cuthttps = pagename[7:]
+findot = cuthttps.find('.')
+sitename = cuthttps[:findot]
+password = sitename[:3] + str(len(sitename)) + str(sitename.count('e')) + '!'
+print("생성된 비밀번호는 {}입니다.".format(password))
+
